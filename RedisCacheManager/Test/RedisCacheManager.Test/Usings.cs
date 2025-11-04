2a298c9bce12ffc63be1d87bdaef775fdea76299
amirhossein <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-11-04 15:50:18 +0330

    Create Usings.cs

---
+++ RedisCacheManager/Test/RedisCacheManager.Test/Usings.cs
@@ -0,0 +1,5 @@
+global using CacheManager.Core;
+global using CacheManager.Tests.Fixtures;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using CacheManager.Abstraction;
